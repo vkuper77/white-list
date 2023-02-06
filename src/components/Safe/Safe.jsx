a3635d0d4322c7d6c8f0beded27f164efe3c1f07
Vitali <kupratsevich@msa-it.ru>
2023-02-06 13:33:31 +0300

    feat: add Safe

--- src/components/Safe/Safe.jsx
+++ src/components/Safe/Safe.jsx
@@ -5,11 +5,11 @@ const Safe = () => {
                 <h1 className={styles.card__title}>Safe</h1>
                 <p className={styles.card__subtitle}>Helps people to stay HODL</p>
                 <p className={styles.card__subtitle}>through the bear market</p>
-                <div>
-                    <div>
+                <div className={styles.cards}>
+                    <div className={styles.card__left}>
                         <p className={styles.card__subtitle}>wallet balance:</p>
-                        <p className={styles.card__subtitle}>2.663 ETH</p>
-                        <a>add</a>
+                        <p className={`${styles.card__subtitle} ${styles.text__balance}`}>2.663 ETH</p>
+                        <a className={styles.button}>add</a>
                     </div>
                     <div>
                         <p className={styles.card__right__title}>9:22:26:8</p>
@@ -17,6 +17,10 @@ const Safe = () => {
                         <p className={styles.card__subtitle}>to your money is unlocked</p>
                     </div>
                 </div>
+                <div className={styles.ellipse__header} />
+                <div className={styles.background__card}/>
+                <div className={styles.ellipse__footer__left} />
+                <div className={styles.ellipse__footer__right} />
             </div>)
 }
 
