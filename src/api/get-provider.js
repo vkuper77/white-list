3b05d6cb88b96dbc88dbea2c6de30d9dae53167f
Vitali <kupratsevich@msa-it.ru>
2023-02-07 15:26:36 +0300

    feat: add provider

---
+++ src/api/get-provider.js
@@ -0,0 +1,18 @@
+import Web3 from "web3";
+
+export async function getProvider() {
+    let provider = null
+    if(window.ethereum) {
+      provider = window.ethereum
+      try {
+          await provider.request({method: 'eth_requestAccounts'})
+      } catch { 
+          console.error("User denied accounts access!")
+      }
+    } else if(window.web3) {
+      provider = window.web3.currentProvider
+    } else if(!process.env.production) {
+      provider = new Web3.providers.HttpProvider('http://localhost:7545')
+    }
+    return provider
+} 
--- src/hooks/use-load-provider.js
+++ src/hooks/use-load-provider.js
@@ -1,36 +1,24 @@
 import { useEffect, useState } from "react";
 import Web3 from "web3";
+import { getProvider } from "../api/get-provider";
 
-const initSate = {
+const initState = {
     provider: null,
     web3: null 
-  }
+  } 
 
 export default function useLoadProvider() {
-    const [web3Api, setWeb3Api] = useState(initSate)
-      useEffect(() => {
+    const [web3Api, setWeb3Api] = useState(initState)
+    useEffect(() => {
         async function loadProvider() {
-          let provider = null
-          if(window.ethereum) {
-            provider = window.ethereum
-            try {
-                await provider.enable()
-            } catch { 
-                console.error("User denied accounts access!")
-            }
-          } else if(window.web3) {
-            provider = window.web3.currentProvider
-          } else if(!process.env.production) {
-            provider = new Web3.providers.HttpProvider('http://localhost:7545')
-          }
-          setWeb3Api({
-            provider,
-            web3: new Web3(provider)
-          })
+            const provider = await getProvider()
+            setWeb3Api({
+                provider,
+                web3: new Web3(provider)
+            })
         }
-
         loadProvider()
-      }, [])
+    }, [])
 
     return web3Api
 }
