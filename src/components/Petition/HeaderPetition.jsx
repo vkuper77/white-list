ca345a3fa65f38c9a11c0fad295d0f1d17cc2792
Vitali <kupratsevich@msa-it.ru>
2023-02-07 14:02:12 +0300

    feat: add web3

--- src/components/Petition/HeaderPetition.jsx
+++ src/components/Petition/HeaderPetition.jsx
@@ -1,11 +1,24 @@
 import styles from "@/styles/Petition.module.css"
+import { useEffect } from "react";
 
 const HeaderPetition = () => {
+
+    useEffect(() => {
+        function loadProvider(){
+        
+         }
+     
+         loadProvider()
+       }, [])
+
     return <>
             <div className={styles.header_container}>
                 <p className={styles.small_title}>public</p>
                 <h1 className={styles.title}>petition</h1>
-                <a className={styles.button}>sign</a>
+                <a onClick={async () => {
+                    const accounts = await window.ethereum.request({method: 'eth_requestAccounts'})
+                    console.log(accounts)
+                }} className={styles.button}>sign</a>
                 <p className={styles.text}>signs quantity: 1</p>
             </div>
         </> 
