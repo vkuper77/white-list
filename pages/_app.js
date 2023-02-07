453484241a7761321282c70dacf34f74605bca75
Vitali <kupratsevich@msa-it.ru>
2023-02-07 15:48:21 +0300

    refactor: app

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,24 @@
 import '@/styles/globals.css'
+import useLoadProvider from "@/src/hooks/use-load-provider";
+import { useEffect, useState } from "react";
 
 export default function App({ Component, pageProps }) {
+  const [account, setAccount] = useState(null)
+  const {web3} = useLoadProvider()
+
+  useEffect(() => {
+    async function getAccounts() {
+      try{
+        const [ score ] = await web3.eth.getAccounts()
+        setAccount(score)
+      } catch{
+        console.error('Failed to get wallet')
+      }
+    }
+    Boolean(web3) && getAccounts()
+  }, [web3])
+
+  console.log(account)
+ 
   return <Component {...pageProps} />
 }
--- pages/index.js
+++ pages/index.js
@@ -5,29 +5,8 @@ import Form from "@/src/components/Form/Form";
 import NavBar from "@/src/components/NavBar/NavBar";
 import Petition from "@/src/components/Petition/Petition";
 import Safe from "@/src/components/Safe/Safe";
-import useLoadProvider from "@/src/hooks/use-load-provider";
-import { useEffect, useState } from "react";
-
 
 export default function Home() {
-  const [account, setAccount] = useState(null)
-  const {web3} = useLoadProvider()
-
-  useEffect(() => {
-    async function getAccounts() {
-      try{
-        const [ score ] = await web3.eth.getAccounts()
-        setAccount(score)
-      } catch{
-        console.error('Failed to get wallet')
-      }
-    }
-    Boolean(web3) && getAccounts()
-  }, [web3])
-
-  console.log(account)
- 
-  
   return (
       <div className='wrapper'>
         <NavBar />
