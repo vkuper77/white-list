fcbf28d913e152d50c78949d9ac99212608c0256
Vitali <kupratsevich@msa-it.ru>
2023-02-08 13:14:40 +0300

    refactor: app-context

--- src/context/app-context.js
+++ src/context/app-context.js
@@ -1,6 +1,7 @@
 import useLoadProvider from "@/src/hooks/use-load-provider";
 import { useEffect, useState, createContext } from "react";
 import { accountListener } from "../utils/account-listener";
+import { attempt } from "../utils/error-listener";
 
 export const AppContext = createContext(null)
 
@@ -19,69 +20,39 @@ export function AppProvider({children}) {
     !!provider && accountListener(provider, setAccount)
   }, [provider])
 
-
   useEffect(() => {
     !!web3 && (async () => {
-      try {
-        const [ acc ] = await web3.eth.getAccounts()
-        const contractBalance = await web3.eth.getBalance(contract.address)
+        const [ acc ] = await attempt(web3.eth.getAccounts())
+        const contractBalance = await attempt(web3.eth.getBalance(contract.address))
         setAccount(acc)
         setBalance(web3.utils.fromWei(contractBalance, 'ether'))
-      } catch(e) {
-        console.error(e)
-      }
     })()
   }, [web3])
 
   useEffect(() => {
     !!web3 && (async () => {
-      try {
-        const value = await web3.eth.getBalance(contract.address)
-        setBalance(web3.utils.fromWei(value, 'ether'))
-      } catch {
-        console.error('Failed to get wallet')
-      }
+      const value = await attempt(web3.eth.getBalance(contract.address))
+      !!value && setBalance(web3.utils.fromWei(value, 'ether'))
     })()
   }, [shouldReload])
 
-  async function getStorage() {
-    const key = await web3.eth.getBlock('latest')
-    console.log(key)
-  }
-
-  async function recordInWhiteList(){
-    try {
-      const res = await contract.recordInWhiteList({from: account})
-    } catch(e) {
-      console.error(e)
-    }
+  async function recordInWhiteList() {
+      await attempt(contract.recordInWhiteList({from: account}))
   }
 
   async function sign() {
-    try {
-      const res = await contract.doSign({from: account})
-    } catch(e) {
-      console.error(e)
-    }
+    await attempt(contract.doSign({from: account}))
   }
 
   async function add(){
-    try {
-      const res = await contract.putInSafe({from: account, value: web3.utils.toWei('1', 'ether')})
+      await attempt(contract.putInSafe({from: account, value: web3.utils.toWei('1', 'ether')}))
       reloadEffect()
-    } catch(e) {
-      console.error(e)
-    }
   }
 
   async function getFromSafe(){
-    try {
-      const res = await contract.getFromSafe({from: account})
-      reloadEffect()
-    } catch(e) {
-      console.error(e)
-    }
+    await attempt(contract.getFromSafe({from: account}))
+    reloadEffect()
   }
 
-    return <AppContext.Provider value={{getStorage, recordInWhiteList, sign, add, getFromSafe, balance }}>{children}</AppContext.Provider>
+    return <AppContext.Provider value={{recordInWhiteList, sign, add, getFromSafe, balance }}>{children}</AppContext.Provider>
 }
