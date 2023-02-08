217a729ecd2941e8d2464b82c140b2759ecdec96
Vitali <kupratsevich@msa-it.ru>
2023-02-08 13:53:20 +0300

    reafactor: app-context

--- src/utils/account-listener.js
+++ src/utils/account-listener.js
@@ -1,5 +1,13 @@
 export function accountListener(provider, callback) {
+    
     provider.on('accountsChanged', (accounts) => {
         callback(accounts[0])
+    })
+
+    provider._jsonRpcConnection.events.on('notification', payload => {
+        const { method } = payload
+        if(method === 'metamask_unlockStateChanged') {
+            callback(null)
+        }
     })
 }
