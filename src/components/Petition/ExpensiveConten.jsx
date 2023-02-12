9b1e1595091057b273e872db6cb0ee16f702d067
Vitali <kupratsevich@msa-it.ru>
2023-02-12 21:56:58 +0300

    feat: add => you are logged in

--- src/components/Petition/ExpensiveConten.jsx
+++ src/components/Petition/ExpensiveConten.jsx
@@ -1,11 +1,11 @@
 import {memo} from 'react'
 import styles from "@/styles/Petition.module.css"
 
-const ExpensiveConten = ({collback}) => (
+const ExpensiveConten = ({collback, isSigned}) => (
     <div className={styles.header_container}>
         <p className={styles.small_title}>public</p>
         <h1 className={styles.title}>petition</h1>
-        <a onClick={collback} className={styles.button}>sign</a>
+        <a onClick={collback} className={styles.button}>{isSigned ? 'you are logged in' : 'sign'}</a>
         <p className={styles.text}>signs quantity: 1</p>
     </div>
 )
