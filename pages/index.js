07f9e079c276bdf75a4f1d5958cfabfd90f8a629
Vitali <kupratsevich@msa-it.ru>
2023-02-17 13:10:18 +0300

    feat: hello branch

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import CashMachine from "@/src/components/Cash Machine/CashMachine";
 import Description from "@/src/components/Description/Description";
 import Form from "@/src/components/Form/Form";
 import NavBar from "@/src/components/NavBar/NavBar";
+import Notification from "@/src/components/UI/Notification";
 import Petition from "@/src/components/Petition/Petition";
 import Safe from "@/src/components/Safe/Safe";
 
@@ -18,6 +19,7 @@ export default function Home() {
         </div>
         <Description />
         <Form />
+        <Notification />
       </div>
   )
 }
--- src/components/Petition/Petition.jsx
+++ src/components/Petition/Petition.jsx
@@ -1,14 +1,13 @@
 import styles from "@/styles/Petition.module.css"
 import HeaderPetition from "./HeaderPetition"
 import MainPetition from "./MainPetition"
-import Notification from "./Notification"
+import Notification from "../UI/Notification"
 
 const Petition = () => {
     return (
         <div id='petition' className={styles.container}>
             <HeaderPetition />
             <MainPetition />
-            <Notification />
         </div>
     )
 }
