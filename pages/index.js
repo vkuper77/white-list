5a457ad6dff3c80db638d9dc24480cdfc538f0d9
Vitali <vkuper7@gmail.com>
2023-04-30 17:08:45 +0300

    feat: add menu header

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,8 @@
+import NavBar from "@/src/components/NavBar/NavBar";
 import About from "@/src/components/About/About";
 import CashMachine from "@/src/components/Cash Machine/CashMachine";
 import Description from "@/src/components/Description/Description";
 import Form from "@/src/components/Form/Form";
-import NavBar from "@/src/components/NavBar/NavBar";
 import Notification from "@/src/components/UI/Notification";
 import Petition from "@/src/components/Petition/Petition";
 import Safe from "@/src/components/Safe/Safe";
@@ -11,18 +11,18 @@ import Tips from "@/src/components/UI/Tips";
 export default function Home() {
   return (
       <div className='wrapper'>
-        <Tips />
+        {/* <Tips /> */}
         <NavBar />
-        <About />
-        <Petition />
-        <div className="main__container">
-          <Safe>
-            <CashMachine/>
-          </Safe>
-        </div>
-        <Description />
-        <Form />
-        <Notification />
+        {/* <About /> */}
+        {/* <Petition /> */}
+        {/* <div className="main__container"> */}
+          {/* <Safe> */}
+            {/* <CashMachine/> */}
+          {/* </Safe> */}
+        {/* </div> */}
+        {/* <Description /> */}
+        {/* <Form /> */}
+        {/* <Notification /> */}
       </div>
   )
 }
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,19 +1,47 @@
+import { NAV_BAR_PAGES } from "@/src/constants/content-page";
+import styles from "@/styles/NavBar.module.css";
 
-import { NAV_BAR_PAGES } from "@/src/constants/content-page"
-import styles from "@/styles/NavBar.module.css"
+const NavBar = () => {
+  return (
+    <header className={styles.header}>
+      <nav className={`${styles.header__menu} _content`}>
+        <ul className={styles.menu__list}>
+          {NAV_BAR_PAGES.map((el) => {
+            return (
+              <li
+                key={el.id}
+                className={`${styles.menu__item} ${
+                  el.id === 3 ? styles.menu__active : null
+                }`}
+              >
+                <a
+                  href={el.href}
+                  className={`${styles.link} ${
+                    el.id === 3 ? styles.menu__active : null
+                  }`}
+                >
+                  {el.title}
+                </a>
+              </li>
+            );
+          })}
+        </ul>
+      </nav>
+    </header>
+  );
+};
 
-const NavBar = () =>  (
-    <ul className={styles.container}>
-        {NAV_BAR_PAGES.map((page, idx, arr) =>
-            (<li key={page.id} className={
-                 !!idx && idx !== arr.length -1 ?
-                 page.id === 4 ? 
-                 styles.indentation__specific : 
-                 styles.indentation__default :
-                 styles.indentation__specific_wrap}>
-                <a href={page.href} className={styles.link}>{page.title}</a>
-            </li>)
-        )}
-    </ul>)
+// <ul className={styles.container}>
+//     {NAV_BAR_PAGES.map((page, idx, arr) =>
+//         (<li key={page.id} className={
+//              !!idx && idx !== arr.length -1 ?
+//              page.id === 4 ?
+//              styles.indentation__specific :
+//              styles.indentation__default :
+//              styles.indentation__specific_wrap}>
+//             <a href={page.href} className={styles.link}>{page.title}</a>
+//         </li>)
+//     )}
+// </ul>)
 
-export default NavBar 
+export default NavBar;
