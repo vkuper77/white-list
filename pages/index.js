db72dd476599fa327245b7edba7ace60856e3064
Vitali <vkuper7@gmail.com>
2023-05-01 14:31:28 +0300

    refactor: page

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,6 @@ import NavBar from "@/src/components/NavBar/NavBar";
 import About from "@/src/components/About/About";
 import CashMachine from "@/src/components/Cash Machine/CashMachine";
 import Description from "@/src/components/Description/Description";
-import Form from "@/src/components/Form/Form";
 import Notification from "@/src/components/UI/Notification";
 import Petition from "@/src/components/Petition/Petition";
 import Safe from "@/src/components/Safe/Safe";
